44dc13c56e44838f9e3e7aa33007abb91bb16ace
Fairleigh Dickinson Software Design <55007696+lvallone3315@users.noreply.github.com>
2024-09-11 21:50:01 -0400

    Added a line on github

--- McJiraWalkThrough/McJiraWalkThrough.cpp
+++ McJiraWalkThrough/McJiraWalkThrough.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "   Hello from Lee\n";
+    std::cout << "    Added a print on github directly\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
